c60ad662e700d5858b30bfe387151621c4a65318
jmclean-coder <joshmclena@gmail.com>
2020-03-10 22:50:25 -0700

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,15 @@
+class Dog
+    def name=(dogs_name)
+    @this_dogs_name = dogs_name
+    end
+    #take in an argument of a dog's name and set 
+    #that name equal to a variable
+    # this method is a "setter" 
+    
+    def name
+    @this_dogs_name
+    end
+    #this methord reports/reads the name
+    #this method is a "getter"
+end
+
